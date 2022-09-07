f21acff265c120447869e5ae1e22e42763c5d1cb
SunnyCoveDD <dimakhov174@gmail.com>
2022-09-07 17:53:59 +0500

    Version 1.0.0 (Login&Register)

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/login',[UserController::class,'login'])->name('login');
+Route::post('/login',[UserController::class,'loginPost']);
+
+Route::get('/register',[UserController::class,'register'])->name('register');
+Route::post('/register',[UserController::class,'registerPost']);
